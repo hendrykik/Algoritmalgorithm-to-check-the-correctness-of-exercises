83f832bf35c0b06f22af03afdeb059eec723aa78
hendrykik <Hendrykik@wp.pl>
2023-10-23 22:15:57 +0200

    Knee Safety

--- squat/squat_Side_Depth.py
+++ squat/squat_Side_Depth.py
@@ -1,5 +1,6 @@
 import cv2
 import mediapipe as mp
+import numpy as np
 
 # Inicjalizacja obiektu MediaPipe
 mp_drawing = mp.solutions.drawing_utils
@@ -44,10 +45,18 @@ point_names = {
 
 def main():
     # Inicjalizacja kamery wideo
-    cap = cv2.VideoCapture('/Users/janzemlo/Desktop/Inzynierka/krotkie.mp4')
+    cap = cv2.VideoCapture('//Users/janzemlo/Inzynierka/krotkie.mp4')
+
+    # max_width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+    # max_height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    #
+    # print(max_width, max_height)
+
+    max_depth_hip = 0
+    max_depth_knee = 0
 
     # Inicjalizacja modelu śledzenia postawy
-    with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+    with mp_pose.Pose(min_detection_confidence=0.7, min_tracking_confidence=0.7) as pose:
         while cap.isOpened():
             # Odczyt klatki z kamery
             ret, frame = cap.read()
@@ -61,15 +70,27 @@ def main():
             results = pose.process(frame_rgb)
 
             # Rysowanie szkieletu na obrazie wraz z nazwami punktów
+
+
             if results.pose_landmarks:
                 for id, landmark in enumerate(results.pose_landmarks.landmark):
-                    h, w, c = frame.shape
-                    cx, cy = int(landmark.x * w), int(landmark.y * h)
-                    cv2.circle(frame, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
-                    point_name = point_names.get(id, "unknown")
-                    cv2.putText(frame, point_name, (cx, cy), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
+                    if id in {25, 27, 29, 11, 23}:
+                        h, w, c = frame.shape
+                        cx, cy = int(landmark.x * w), int(landmark.y * h)
+                        cv2.circle(frame, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
+                        point_name = point_names.get(id, "unknown")
+                        cv2.putText(frame, point_name, (cx, cy), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
+                        if id == 23:
+                            if max_depth_hip < int(cy):
+                                max_depth_hip = int(cy)
+                                max_depth_knee = int(results.pose_landmarks.landmark[25].y * h)
+                                # print(max_depth_hip)
+                                # print(int(results.pose_landmarks.landmark[25].y * h))
+                                #max_depth_knee = int(cy)
 
             # Wyświetlenie klatki z szkieletem
+            cv2.circle(frame, (500, 827), 50, (255, 0, 0), thickness=-1)  # Niebieski kolor: (255, 0, 0)
+
             cv2.imshow('Pose Estimation', frame)
 
             # Przerwanie pętli po naciśnięciu klawisza 'q'
@@ -77,6 +98,14 @@ def main():
                 break
 
         # Zwolnienie zasobów
+        print(max_depth_hip)
+        print(max_depth_knee)
+
+        if max_depth_hip < max_depth_knee:
+            print("Poprawna głębokość przysiadu")
+        else:
+            print("Niepoprawna głębokość przysiadu")
+
         cap.release()
         cv2.destroyAllWindows()
 
